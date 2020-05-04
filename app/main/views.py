ee81d4268020b0907914437b8241a1e5e2038666
Brenda Wanjiku <brendawanjiku@Brendas-MacBook-Air.local>
2020-05-04 13:51:38 +0300

    Creates Comment db.model. Creates category funtion in views.py to retrieve pitches.

--- app/main/views.py
+++ app/main/views.py
@@ -1,19 +1,28 @@
+from ..models import User,Pitch
 from flask import render_template,redirect,url_for
 from . import main
+from .. import db
 from flask_login import login_required
 
 
 @main.route('/')
 def index():
-
+    tech = Pitch.get_pitches('Technology')
     title = 'Peaches'
 
-    return render_template('index.html', title = title)
+
+    return render_template('index.html',title = title, tech = tech)
 
 
 @main.route('/pitches/<category>')
 def categories(category):
+    title = "Category"
+    phil = Pitch.get_pitches('Philosophy')
 
-    return render_template('pitches.html')
+    return render_template('pitches.html', title = title, phil = phil)
+
+
+
+
 
 
--- app/models.py
+++ app/models.py
@@ -6,10 +6,26 @@ from . import login_manager
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(225))
-    email = db.Column(db.String(225), unique = True, index = True)
-    pass_secure = db.Column(db.String(225))
-    pitches = db.relationship('Pitch', backref = 'pitch', lazy = "dynamic")
+    name = db.Column(db.String(225))
+    email = db.Column(db.String)
+    bio = db.Column(db.String)
+    profile_pic  = db.Column(db.String)
+
+    pitches = db.relationship('Pitch', backref = 'name', lazy = "dynamic")
+    comments = db.relationship('Comment', backref = 'name', lazy = "dynamic")
+
+   
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+
+    @classmethod
+    def get_users(cls, id):
+        users_list = User.query.filter_by(user_id = id)
+        return users_list
 
 
     @property
@@ -18,11 +34,12 @@ class User(UserMixin, db.Model):
 
     @password.setter
     def password(self, password):
-        self.pass_secure = generate_password_hash(password)
+        self.pass_code = generate_password_hash(password)
 
 
     def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
+        return check_password_hash(self.pass_code,password)
+
 
     @login_manager.user_loader
     def load_user(user_id):
@@ -30,18 +47,44 @@ class User(UserMixin, db.Model):
 
 
     def __repr__(self):
-        return f'User {self.username}'
+        return f'User {self.name}'
 
 
 class Pitch(db.Model):
     __tablename__ = 'pitches'
+
     id = db.Column(db.Integer, primary_key = True)
-    pitch_title = db.Column(db.String(225))
-    pitch_content = db.Column(db.String(225))
-    pitch_category = db.Column(db.String(225))
+    category = db.Column(db.String)
+    title = db.Column(db.String) 
+    content = db.Column(db.String)
 
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    comments = db.relationship('Comment', backref = 'pitch_id', lazy = "dynamic")
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitches(cls, category):
+        pitches_list = Pitch.query.filter_by(category = category)
+        return pitches_list
+    
+
 
     def __repr__(self):
-        return f'User {self.username}'
+        return f'User {self.name}'
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key = True)
+    mention = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    
 
