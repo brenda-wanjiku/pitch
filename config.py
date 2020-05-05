16fd53cefad1af213afe2a0cbf2b60c3c8fa035f
Brenda Wanjiku <brendawanjiku@Brendas-MacBook-Air.local>
2020-05-05 14:27:28 +0300

    Final commits

--- config.py
+++ config.py
@@ -15,8 +15,9 @@ class Config:
     SIMPLEMDE_USE_CDN = True
 
 class ProdConfig(Config):
-    pass
+      SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
+      
 class DevConfig(Config):
     DEBUG = True
 
